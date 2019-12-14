ecea309fe8b06ed73af251715112efc60a043106
kellyeryan <kellyeryan@gmail.com>
2019-12-14 16:03:03 -0500

    Resolve rubocop offenses

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,33 @@
 # (or created alongside the database with db:setup).
 #
 
-Patron.create(first_name: "Wilson", last_name: "Contreras", library_card_number: 3689221971, password: "cust4gen1ve")
-Patron.create(first_name: "Anthony", last_name: "Rizzo", library_card_number: 2414254605, password: "gauc5fur1drod")
-Patron.create(first_name: "Kris", last_name: "Bryant", library_card_number: 9935025279, password: "zort9rheg8tarm")
-Patron.create(first_name: "Kyle", last_name: "Schwarber", library_card_number: 2974850008, password: "gnos1gnef9zok")
-Patron.create(first_name: "Javier", last_name: "Baez", library_card_number: 2080286161, password: "fis1yee4jet")
+Patron.create(
+  first_name: "Wilson",
+  last_name: "Contreras",
+  library_card_number: 3_689_221_971,
+  password: "cust4gen1ve"
+)
+Patron.create(
+  first_name: "Anthony",
+  last_name: "Rizzo",
+  library_card_number: 2_414_254_605,
+  password: "gauc5fur1drod"
+)
+Patron.create(
+  first_name: "Kris",
+  last_name: "Bryant",
+  library_card_number: 9_935_025_279,
+  password: "zort9rheg8tarm"
+)
+Patron.create(
+  first_name: "Kyle",
+  last_name: "Schwarber",
+  library_card_number: 2_974_850_008,
+  password: "gnos1gnef9zok"
+)
+Patron.create(
+  first_name: "Javier",
+  last_name: "Baez",
+  library_card_number: 2_080_286_161,
+  password: "fis1yee4jet"
+)
