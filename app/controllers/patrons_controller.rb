097597dde61c1d94b696c3cf3c5bc9d583155156
kellyeryan <kellyeryan@gmail.com>
2019-12-16 10:37:05 -0500

    Work on sign up form and routes

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -8,6 +8,23 @@ class PatronsController < ApplicationController
   def new; end
 
   def create
+    patron = Patron.new(patron_params)
+    if patron.save
+      session[:patron_id] = patron.id
+      redirect_to patron_path(patron)
+    else
+      redirect_to signup_path
+    end
+  end
 
+private
+
+  def patron_params
+    params.require(:patron).permit(
+      :first_name,
+      :last_name,
+      :password,
+      :pa_resident
+    )
   end
 end
