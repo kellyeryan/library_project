3f6f3700f363d958d0e63ea535b1f1e22477a367
kellyeryan <kellyeryan@gmail.com>
2019-12-21 20:49:30 -0500

    Write book loans controller so it creates a book loan and create show page for book loans

--- app/controllers/book_loans_controller.rb
+++ app/controllers/book_loans_controller.rb
@@ -1,25 +1,16 @@
 # frozen_string_literal: true
 
 class BookLoansController < ApplicationController
-  def index
-    @book_loans = BookLoan.all
-  end
-
-  def new
-    @book_loan = BookLoan.new
-    @book_loan.patron_id =
-  end
+  # def index
+  #   @book_loans = BookLoan.all
+  # end
 
   def create
-    BookLoan.new(book_loan_params)
+    @book_loan = BookLoan.create(book_id: params[:book_id], patron_id: session[:patron_id])
+    redirect_to book_loan_path(@book_loan)
   end
 
   def show
     @book_loan = BookLoan.find(params[:id])
-  end
-
-private
-  def book_loan_params
-    params.require(:book_loan).permit(:book_id, :patron_id, :checked_out)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :authors, only: [:index, :new, :create, :show]
 
   resources :patrons, only: [:show, :new, :create]
-  resources :book_loans, only: [:index, :new, :show]
+  resources :book_loans, only: [:index, :create, :show]
   root "static_pages#home"
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
