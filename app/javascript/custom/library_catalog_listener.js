39f9e639d96118da37f570c524f5eae3a2761c64
kellyeryan <kellyeryan@gmail.com>
2020-02-17 15:15:52 -0500

    Work on library catalog listener

--- app/javascript/custom/library_catalog_listener.js
+++ app/javascript/custom/library_catalog_listener.js
@@ -1,1 +1,12 @@
-library_catalog_listener.js
+$(function(){
+  $(".js-browse").on("click", function(event){
+    event.preventDefault();
+    console.log(event)
+    let catalogURL = event.target.href
+    console.log(catalogURL)
+    $.get(`${catalogURL}`, function(data){
+      console.log(data)
+      $(".js-catalog").append($(data))
+    })
+  })
+})
